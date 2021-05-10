fa40a1c66d1d0359e8f14238896d0acab4a6f925
jbisits <75812103+jbisits@users.noreply.github.com>
2021-05-10 10:43:35 +1000

    Add function area_tracer_patch to `MeasureMixing.jl`.
    This is a function to calculate the second moment evolution of the tracer patch as function of time. It is used by Jan in a 2020 paper and was originally derived by Garret (1983). Yet to be finished.

--- Modules/MeasureMixing.jl
+++ Modules/MeasureMixing.jl
@@ -10,7 +10,7 @@ module MeasureMixing
 export
     conc_var!
 
-using Distributions
+using Distributions, GeophysicalFlows
 
 #Calculate the variance of the tracer concentration.
 function conc_var!(conc_var, prob) 
@@ -26,5 +26,28 @@ function conc_var!(conc_var, prob)
 end
 
 #Second moment evolution of tracer patch
+
+function area_tracer_patch(AD_prob, QG_prob, Kₛ)
+
+    α = 0.5
+    params = QG_prob.params
+    t = AD_prob.clock.t
+    #Q = total amount of tracer thought not sure how to define this.
+
+    #∂xu and ∂yv can be found in Fourier space, then transformed to physical space
+    uh, vh = QG_prob.vars.uh, QG_prob.vars.vh
+    uhx = im * QG_prob.grid.kr * uh
+    vhy = im * QG_prob.grid.l * vh
+
+    ux, vy = QG_prob.vars.u, QG_prob.vars.v #Use these as dummy variables
+    MultiLayerQG.invtransform!(ux, uhx, params)
+    MultiLayerQG.invtransform!(vy, vhy, params)
+
+    γ = sqrt(ux.^2 + vy.^2) #Presumably want this as a single number so take the mean?
+
+    Aₜ = @. π * (Kₛ / γ) * exp(α * γ * (t - 0.25 / γ))
+
+    var_patch = Q^2 * (2 * Aₜ)^(-1)
+end
 
 end #module
