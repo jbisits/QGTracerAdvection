1cd9b7569870bc662eeca574b2f5d6d9d137b964
Josef Bisits <75812103+jbisits@users.noreply.github.com>
2021-05-12 10:08:52 +1000

    Update ensemble_of_tracers.jl
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>

--- ensemble_of_tracers.jl
+++ ensemble_of_tracers.jl
@@ -56,7 +56,7 @@ q_i  = irfft(qh_i, QG_prob.grid.nx, (1, 2))                    # only apply irff
 #Set diffusivity
 κ = 0.01
 
-#Set number of tracer problems, delay time of placing tracer into the flow, and blank arrays for shortcuts.
+#Set number of tracer problems, `n`, the delay time of placing tracer into the flow, and the empy arrays for shortcuts.
 n = 10
 AD_probs = Array{FourierFlows.Problem}(undef, n)
 QG_probs = Array{FourierFlows.Problem}(undef, n)
