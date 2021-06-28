46f2f693927ceba151c27e10dd45b8d3fc875319
jbisits <75812103+jbisits@users.noreply.github.com>
2021-06-28 21:12:14 +1000

    Update DataAnalysis.jl

--- Honours thesis/Experiment/Output/Simulation: Domain = 500000, Res = 128/DataAnalysis.jl
+++ Honours thesis/Experiment/Output/Simulation: Domain = 500000, Res = 128/DataAnalysis.jl
@@ -9,9 +9,8 @@ histplots = MeasureMixing.hist_plot(data)
 #Extract tracer plots from the simulation
 uppertracerplots = data["TracerPlots"]
 
-uppertracer = plot(uppertracerplots[1][1], uppertracerplots[1][2], uppertracerplots[1][3], uppertracerplots[1][4],
-                    uppertracerplots[1][5],uppertracerplots[1][6])
-upperhist = plot(histplots[1][1], histplots[1][2], histplots[1][3], histplots[1][4], histplots[1][5], histplots[1][6])
+uppertracer = plot(uppertracerplots[1]...)
+upperhist = plot(histplots[1]...)
 plot(uppertracer, upperhist, layout=(2, 1), size = (1200, 1200))
 
 ConcVsArea = MeasureMixing.concarea_animate(data, nsteps)
