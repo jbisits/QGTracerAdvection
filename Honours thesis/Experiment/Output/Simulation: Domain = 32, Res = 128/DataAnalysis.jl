6c96708e0d15be844cb887435555a019c0f9cef8
jbisits <75812103+jbisits@users.noreply.github.com>
2021-06-28 21:08:12 +1000

    Add `...` to the plots of arrays in data analysis.
    As I go through other scripts I will update the way I have hard coded the plots to display things to this, rather than listing the elements of the array as the `...` is replacing

--- Honours thesis/Experiment/Output/Simulation: Domain = 32, Res = 128/DataAnalysis.jl
+++ Honours thesis/Experiment/Output/Simulation: Domain = 32, Res = 128/DataAnalysis.jl
@@ -12,16 +12,14 @@ histplots = hist_plot(data)
 tracerplots = tracer_plot(data)
 
 #Plot heatmaps and histograms togehter.
-uppertacer = plot(tracerplots[1][1], tracerplots[1][2], tracerplots[1][3], tracerplots[1][4],
-                    tracerplots[1][5],tracerplots[1][6])
+uppertacer = plot(tracerplots[1]...)
 
-upperhist = plot(histplots[1][1], histplots[1][2], histplots[1][3], histplots[1][4], histplots[1][5], histplots[1][6])
+upperhist = plot(histplots[1]...)
 plot(uppertacer, upperhist, layout=(2, 1), size = (1200, 1200))
 
-lowertracer = plot(tracerplots[2][1], tracerplots[2][2], tracerplots[2][3], tracerplots[2][4],
-                    tracerplots[2][5],tracerplots[2][6])
+lowertracer = plot(tracerplots[2]...)
 
-lowerhist = plot(histplots[2][1], histplots[2][2], histplots[2][3], histplots[2][4], histplots[2][5], histplots[2][6])
+lowerhist = plot(histplots[2]...)
 plot(lowertracer, lowerhist, layout=(2, 1), size = (1200, 1200))
 
 #Time vector for plotting
