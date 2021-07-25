0c889444f909f58098d7d0c94b804ca4dd9aeb45
jbisits <75812103+jbisits@users.noreply.github.com>
2021-07-25 19:24:49 +1000

    Set to run ensemble of 10 problems with strip initial condition

--- Honours thesis/Experiment/EnsembleAdvectionDiffsuionSim.jl
+++ Honours thesis/Experiment/EnsembleAdvectionDiffsuionSim.jl
@@ -7,18 +7,20 @@ cd(SimPath)
 include("PackageSetup.jl")
 
 #Import a flow that has already been set up from the Flows folder
-include("Flows/FlowSetup_nondim_32domain_64res.jl")
+#include("Flows/FlowSetup_nondim_32domain_64res.jl")
 #include("Flows/FlowSetup_nondim_64domain_128res.jl")
-#include("Flows/FlowSetup_nondim_128domain_256res.jl")
+include("Flows/FlowSetup_nondim_128domain_256res.jl")
+#include("Flows/FlowSetup_nondim_256domain_512res.jl")
 
-nsubs  = 200            #Set the number of steps the simulation takes at each iteration. This is also the frequency that data is saved at.         
-nsteps = 4000           #Set the total amount of time steps the advection-diffusion simulation should run for
+nsubs  = 1           #Set the number of steps the simulation takes at each iteration. This is also the frequency that data is saved at.         
+nsteps = 5000           #Set the total amount of time steps the advection-diffusion simulation should run for
 
 κ = 0.01
 #Set delay times (that is flow for some length of time, then drop tracer in)
 delay_time = Δt̂ * 3000
 #Define number of tracer advection simulations
-ADSims = 2
+ADSims = 10
+save_freq = 50
 
 #This runs a non-parallel simulation where an array of advection-diffusion problems is defined then stepped forward separately with the flow reset each time
 for i ∈ 1:ADSims
@@ -26,12 +28,12 @@ for i ∈ 1:ADSims
     if i == 1
         ADProb = TracerAdvDiff_QG.Problem(;prob = QGProb, delay_time = delay_time, nsubs = nsubs, κ = κ)
         ADSol, ADClock, ADVars, ADParams, ADGrid = ADProb.sol, ADProb.clock, ADProb.vars, ADProb.params, ADProb.grid
-        #Set the input for Gaussian blob initial condition
-        μIC = [0, 0]
-        Σ = [1 0; 0 1]
-        global IC = GaussianBlobIC(μIC, Σ, ADGrid)
-        #File name for saving. Only needs to be done the first time in the loop.
-        global filename = CreateFile(ADProb, IC, nsubs, SimPath; Ensemble = true)
+        #Initial condition
+        μIC = 0
+        σ² = 1
+        IC = GaussianStripIC(μIC, σ², ADGrid)
+        #File name for saving
+        global filename = CreateFile(ADProb, IC, save_freq = 20, SimPath; Ensemble = true)
     else
         #Reset the QG flow
         global QGProb = MultiLayerQG.Problem(nlayers, dev; nx=nx, Lx=Lx̂, f₀=f̂₀, g=ĝ, H=Ĥ, ρ=ρ̂, U=Û, dt=Δt̂, stepper=stepper, μ=μ̂, β=β̂, ν=ν̂)
@@ -52,8 +54,9 @@ for i ∈ 1:ADSims
     #Simulation loop
     while ADClock.step <= nsteps
 
-        if ADClock.step % 1000 == 0
-            println("Step number: ", round(Int, ADClock.step))
+        if ADClock.step % save_freq == 0
+            saveoutput(ADOutput)
+            println("Step number: ", round(Int, ADClock.step), ", saved data")
         end
         saveoutput(ADOutput)
         stepforward!(ADProb, nsubs)
@@ -65,7 +68,7 @@ for i ∈ 1:ADSims
     #Save the number of steps in the simulation
     jldopen(ADOutput.path, "a+") do path
         path["clock/nsteps"] = ADClock.step - 1
-        path["save_freq"] = nsubs
+        path["save_freq"] = save_freq
         if delay_time != 0
             path["delay_time"] = delay_time
         end
