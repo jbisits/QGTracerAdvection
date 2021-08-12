d2719bdf2fc1f42df7b1df63b93775ca8474290d
jbisits <75812103+jbisits@users.noreply.github.com>
2021-08-12 10:48:10 +1000

    Add ensemble flows that are set up on rectangular domain and run ensemble experiment with Gaussian blob

--- Honours thesis/Experiment/EnsembleAdvectionDiffsuionSim.jl
+++ Honours thesis/Experiment/EnsembleAdvectionDiffsuionSim.jl
@@ -6,21 +6,28 @@ cd(SimPath)
 #Load in all the required packages for the simulation
 include("PackageSetup.jl")
 
-ADSims = 5
-#Import a an ensemble of flows
+#Define number of tracer advection simulations
+ADSims = 10
+
+#Import a an ensemble of flows on a square domain
 #include("Flows/EnsembleSquare/EnsembleFlow_32domain_64res.jl")
 #include("Flows/EnsembleSquare/EnsembleFlow_64domain_128res.jl")
-include("Flows/EnsembleSquare/EnsembleFlow_128domain_256res.jl")
+#include("Flows/EnsembleSquare/EnsembleFlow_128domain_256res.jl")
 #include("Flows/EnsembleSquare/EnsembleFlow_256domain_512res.jl")
 
+#Import an ensemble of flows on a rectanglular domain
+#include("Flows/EnsembleRectangle/EnsembleFlow_32_64_domain.jl")
+include("Flows/EnsembleRectangle/EnsembleFlow_32_128_domain.jl")
+#include("Flows/EnsembleRectangle/EnsembleFlow_32_256_domain.jl")
+#include("Flows/EnsembleRectangle/EnsembleFlow_64_128_domain.jl")
+#include("Flows/EnsembleRectangle/EnsembleFlow_64_256_domain.jl")
+
 nsubs  = 1           #Set the number of steps the simulation takes at each iteration. This is also the frequency that data is saved at.         
-nsteps = 5000           #Set the total amount of time steps the advection-diffusion simulation should run for
+nsteps = 10000           #Set the total amount of time steps the advection-diffusion simulation should run for
 
 κ = 0.01
 #Set delay times (that is flow for some length of time, then drop tracer in)
 delay_time = Δt̂ * 3000
-#Define number of tracer advection simulations
-ADSims = 10
 #Set the frequency at which to save data
 save_freq = 50
 
@@ -29,10 +36,14 @@ for i ∈ 1:ADSims
 
     ADProb = TracerAdvDiff_QG.Problem(;prob = QGProbs[i], delay_time = delay_time, nsubs = nsubs, κ = κ)
     ADSol, ADClock, ADVars, ADParams, ADGrid = ADProb.sol, ADProb.clock, ADProb.vars, ADProb.params, ADProb.grid
-    #Initial condition
+    #Set the Gaussian blob initial condition
+    μIC = [0, 0]
+    Σ = [1 0; 0 1]
+    IC = GaussianBlobIC(μIC, Σ, ADGrid)
+    #=Set the Gaussian strip initial condition
     μIC = 0
     σ² = 1
-    IC = GaussianStripIC(μIC, σ², ADGrid)
+    IC = GaussianStripIC(μIC, σ², ADGrid)=#
     #File name for saving, FourierFlows creates a new file each time with _i appended
     filename = CreateFile(ADProb, IC, save_freq, SimPath; Ensemble = true)
 
