ee24d8f7a3fbeab5da69290b1cdd90fbfc646163
jbisits <75812103+jbisits@users.noreply.github.com>
2022-10-18 16:56:04 +1100

    Compute diffusivity using a width of Gaussian

--- Paper plots/ens_mean.jl
+++ Paper plots/ens_mean.jl
@@ -9,6 +9,7 @@ t = [ens_file["snapshots/t/$i"] for i ∈ iterations]
 c₁ = [abs.(ens_file["snapshots/Concentration/$i"][:, :, 1]) for i ∈ iterations]
 c₂ = [abs.(ens_file["snapshots/Concentration/$i"][:, :, 2]) for i ∈ iterations]
 
+close(ens_file)
 ## Animation of ensemble mean concentration
 x,  y  = ens_file["grid/x"], ens_file["grid/y"]
 Lx, Ly = ens_file["grid/Lx"], ens_file["grid/Ly"]
@@ -86,4 +87,117 @@ for i ∈ 1:2, j ∈ 1:3
 
 end
 tracer_plots
-save("ens_tracer_plots.png", tracer_plots)
+#save("ens_tracer_plots.png", tracer_plots)
+
+## Fit a Gaussian to the concentration
+
+## Can use the package `LsqFit.jl`
+
+using LsqFit, GLM
+
+## Fit the concentration for a single time step
+
+x,  y  = ens_file["grid/x"], ens_file["grid/y"]
+xy = hcat(x, y)
+
+function twoD_Gaussian(xy, p)
+    amplitude, x₀, y₀, σ = p
+
+    # creating linear meshgrid from xy
+    x = repeat(xy, outer = (512, 1))[:, 1]
+    y = repeat(xy, inner = (512, 1))[:, 1]
+    g =  amplitude .* exp.( - (1 / (2 * σ^2)) .* (((x .- x₀).^2) + ((y .- y₀).^2)))
+
+    return g[:]
+end
+
+p₀ = [0.01, 3, 2, 0.2]
+conc_vals = reshape(c₁[165], :)
+
+fit_Lsq = LsqFit.curve_fit(twoD_Gaussian, xy, conc_vals, p₀)
+fit_Lsq.param
+
+MvNormal(fit_Lsq.param[2:3], fit_Lsq.param[4])
+test_fit(x, y) = pdf(MvNormal(fit_Lsq.param[2:3], fit_Lsq.param[4]), [x, y])
+
+xgrid, ygrid = ones(length(x)) * x', y * ones(length(y))'
+heatmap(x, y, test_fit.(xgrid, ygrid)')
+
+## Fit the Gaussian for all time steps and save the parameters
+
+params_t = Array{Float64}(undef, length(p₀), length(c₁))
+
+for i ∈ eachindex(c₁)
+
+    @info "Snapshot $i"
+    conc_vals = reshape(c₁[i], :)
+    fit_Lsq = LsqFit.curve_fit(twoD_Gaussian, xy, conc_vals, p₀)
+    @. params_t[:, i] = fit_Lsq.param
+
+end
+
+params_t
+σ²_t = params_t[4, :].^2
+
+## Check linear growth, looks like best bet use from beginning to 
+# around half way through
+lines(t, σ²_t)
+
+lines(t[1:90], σ²_t[1:90])
+
+## Linear model for the linear growth (from this we get diffusivity)
+t_half = t[1:90]
+σ²_t_half = σ²_t[1:90]
+
+t_mat = [ones(length(t_half)) t_half]
+sec_mom_model = lm(t_mat, σ²_t_half)
+
+int, slope = coef(sec_mom_model)
+r2(sec_mom_model)
+
+fig_sec_mom = Figure()
+ax = Axis(fig_sec_mom[1, 1], 
+          xlabel = "t̂", 
+          ylabel = "σ̂²(t)",
+          title = "Growth of second moment fitted to the concentration data")
+
+lines!(ax, t_half, σ²_t_half, label = "Second moment (fitted to the data)")
+lines!(ax, t_half, int .+ t_half .* slope, label = "Linear fit")
+axislegend(ax; position = :lt)
+fig_sec_mom
+
+(slope / 2) * 0.02 * 29862 # = 5559.36 upper layer
+
+# slope / 2 because we are interested in the width of the Gaussian
+# as in Abernathy (2013), 0.5 * (dσ² / dt) = κ
+
+## Same plots as ensemble mean above but using the fitted mean and variance
+# Clearly does not match exactly as this is isotropic but not bad
+plot_font = "CMU Modern Serif"
+plot_fs = 20
+latex_fs = 29
+tracer_plots = Figure(resolution = (1200, 1400), fontsize = plot_fs, font = plot_font)
+plot_steps = 1:31:181
+x̂ = ens_file["grid/x"]
+ŷ = ens_file["grid/y"]
+plot_letters = ["(a)" "(b)" "(c)"; "(d)" "(e)" "(f)"]
+
+newline = "\n"
+ax = [Axis(tracer_plots[i, j],
+        xlabel = L"\hat{x}",
+        xlabelsize = latex_fs,
+        ylabel = L"\hat{y}",
+        ylabelsize = latex_fs,
+        title = L"%$(plot_letters[i, j]) \quad \hat{t} = %$(string(plot_times[i, j]))",
+        titlesize = latex_fs,
+        aspect = 1
+        ) for j ∈ 1:3, i ∈ 1:2]
+
+for (i, axis) in enumerate(ax)
+
+    plot_data = [pdf(MvNormal(params_t[2:3, plot_steps[i]], params_t[4, plot_steps[i]]), [xgrid, ygrid]) for xgrid ∈ x, ygrid ∈ y]
+    CairoMakie.heatmap!(axis, x̂, ŷ, plot_data, colormap = :balance)
+
+end
+
+tracer_plots
